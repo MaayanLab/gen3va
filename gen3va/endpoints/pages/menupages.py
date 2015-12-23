a4d8f2e2d6bf69d1a970c22a228e980e8faa1fa9
Greg <ggundersen@gmail.com>
2015-12-23 13:48:46 -0500

    Moved stats to about page
    Moved stats to about page

--- gen3va/endpoints/pages/menupages.py
+++ gen3va/endpoints/pages/menupages.py
@@ -1,8 +1,11 @@
 """Serves menu pages.
 """
 
+import json
+
 from flask import Blueprint, render_template
 
+from gen3va.db import commondal
 from gen3va.config import Config
 
 
@@ -23,4 +26,8 @@ def getting_started():
 
 @menu_pages.route('/about')
 def about():
-    return render_template('pages/about.html')
+    stats = commondal.get_statistics()
+    stats_json = json.dumps(stats)
+    return render_template('pages/about.html',
+                           stats=stats,
+                           stats_json=stats_json)
