dce0730e25c8d531497ae193d6718bae61631d06
Greg <ggundersen@gmail.com>
2016-04-27 16:17:09 -0400

    STASH: Signature search from a long time ago.

--- gen3va/endpoints/api/signature_search_api.py
+++ gen3va/endpoints/api/signature_search_api.py
@@ -1,13 +1,14 @@
 """Manages finding similar signatures.
 """
 
+import math
 
 from flask import Blueprint, jsonify, request, render_template
-
 import numpy as np
 import pandas as pd
+from scipy.spatial.distance import cosine as scipy_cosine
 
-from substrate import GeneSignature
+from substrate import Gene, GeneSignature
 from gen3va import database
 from gen3va.config import Config
 
@@ -27,7 +28,11 @@ def signature_search():
     if request.method == 'GET':
         return render_template('pages/signature-search.html')
     else:
+        all_sigs_mat = build_matrix_of_all_signatures()
+
+
         all_signatures = database.get_all(GeneSignature)
+        ids_ = list(range(len(all_signatures)))
         scores = np.zeros(len(all_signatures))
         user_list = request.form.get('signature').split('\n')
         tmp = [x.strip().upper().split(',') for x in user_list]
@@ -35,21 +40,49 @@ def signature_search():
 
         A = pd.DataFrame(index=list(user_genes),
                          data=[float(x) for x in user_values])
-
         for i, sig in enumerate(all_signatures):
             if i % 100 == 0:
                 print(i)
-            c = sig.combined_genes
-            genes = [rg.gene.name for rg in c]
-            values = [rg.value for rg in c]
+            ids_[i] = sig.extraction_id
+            cg = sig.combined_genes
+            genes = [rg.gene.name for rg in cg]
+            values = [rg.value for rg in cg]
             B = pd.DataFrame(index=genes, data=values)
-            scores[i] = cosine_similarity(A, B)
+            if not B.index.is_unique:
+                B = B.groupby(B.index).mean()
+            sc = cosine_similarity(A, B)
+            if math.isnan(sc):
+                sc = 0
+            scores[i] = sc
 
-        import pdb; pdb.set_trace()
+        results = zip(ids_, scores)
+        results = sorted(results, key=lambda x: x[1])
+        results.reverse()
         return jsonify({
-            'signature': []
+            'scores': results
         })
 
+
+def build_matrix_of_all_signatures():
+    """
+    """
+    import pdb; pdb.set_trace()
+    all_signatures = database.get_all(GeneSignature)
+    unique_genes = np.array([x.name for x in database.get_all(Gene)])
+    n_rows = len(all_signatures)
+    n_cols = len(unique_genes)
+    mat = np.matrix(np.zeros((n_rows, n_cols)))
+    print ('matrix built')
+
+    for i, sig in enumerate(all_signatures):
+        print(i)
+        cg = sig.combined_genes
+        genes = [rg.gene.name for rg in cg]
+        values = [rg.value for rg in cg]
+        hits = np.in1d(unique_genes, genes)
+        mat[0, np.nonzero(hits)] = values
+
+    import pdb; pdb.set_trace()
 
 
 def overlap(a, b):
@@ -64,7 +97,7 @@ def cosine_similarity(a, b):
     """Calculates the cosine similarity between two vectors of genes.
     """
     A, B = overlap(a, b)
-    num = A.T.dot(B)
-    den = np.sqrt((A.values**2).sum() * (B.values**2).sum())
-    sim = num/den
-    return sim.ix[0,0]
+    if A.empty or B.empty:
+        return 0
+    score = scipy_cosine(A, B)
+    return score
