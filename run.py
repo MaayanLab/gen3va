4fee9f08d6f1b5b3ce140b7f2787cbbb16681876
Greg <ggundersen@gmail.com>
2016-06-29 09:45:56 -0400

    Allow app to run on user-defined port.

--- run.py
+++ run.py
@@ -3,6 +3,14 @@
 # In production, Flask is run by mod_wsgi, which imports the via wsgi.py.
 
 
+import argparse
 from gen3va import app
 
-app.run(debug=True, port=8084, host='0.0.0.0')
+parser = argparse.ArgumentParser()
+parser.add_argument('--port',
+                    default=8084,
+                    type=int,
+                    help='Port for development Flask application.')
+args = parser.parse_args()
+
+app.run(debug=True, port=args.port, host='0.0.0.0')
