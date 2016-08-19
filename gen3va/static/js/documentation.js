813010461d0263c9c1115b5ae70ae90ebd9539c3
Greg <ggundersen@gmail.com>
2016-08-19 15:36:43 -0400

    Smooth scrolling between sections.

--- gen3va/static/js/documentation.js
+++ gen3va/static/js/documentation.js
@@ -1,16 +1,42 @@
 $(function() {
-    var hash = location.hash.replace('#', '');
+
+    var hash = getHash();
     if (hash) {
-        selectH1(hash);
+        highlightSection(hash);
     }
 
     $('#documentation-nav li a').click(function(evt) {
-        var id = $(evt.target).attr('href').split('#')[1];
-        selectH1(id);
+        //debugger;
+        var $this = $(this),
+            id = $this.attr('href').split('#')[1];
+        scrollTo(id);
+        highlightSection(id);
+        addHash(id);
     });
 
-    function selectH1(id) {
+    function highlightSection(id) {
         $('h2').removeClass('selected');
         $('h2#' + id).addClass('selected');
+    }
+
+    function scrollTo(id) {
+        var top  = $('h2#' + id).offset().top;
+        $('html, body')
+            .stop()
+            .animate({ scrollTop: top }, 700, function() {
+                addHash(id);
+            });
+    }
+
+    function addHash(id) {
+        var hash = getHash(),
+            node = $('#' + id);
+        node.attr('id', '');
+        window.location.hash = id;
+        node.attr('id', hash);
+    }
+
+    function getHash() {
+        return window.location.hash.replace('#', '');
     }
 });
